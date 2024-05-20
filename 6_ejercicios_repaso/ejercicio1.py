42ad886bb365fce8b428bc175841daea3367acba
Pepe543235 <joxegp@gmail.com>
2024-05-20 11:32:46 -0600

    primeros 5 ejercicios

---
+++ 6_ejercicios_repaso/ejercicio1.py
@@ -0,0 +1,5 @@
+pais="México"
+continente="Americano"
+
+print(f"El pais en el que vivo es: {pais}, y el continente es {continente}")
+print(f"El tipo de dato de pais es {type(pais)} y el de continente es {type(continente)}")
---
+++ 6_ejercicios_repaso/ejercicio2.py
@@ -0,0 +1,4 @@
+contador=0
+while contador<20:
+    contador=contador+2
+    print(contador)
